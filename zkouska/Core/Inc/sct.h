1b68042362cf1513d9fa4b0de3e718775e367800
Matej Podany <matej@podany.eu>
2023-01-09 11:01:01 +0100

    zkouska_done

---
+++ zkouska/Core/Inc/sct.h
@@ -0,0 +1,18 @@
+/*
+ * sct.h
+ *
+ *  Created on: 12. 10. 2022
+ *      Author: xpodan00
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+
+void sct_init(void); // function declaration
+
+void sct_led(uint32_t value); // function declaration
+
+void sct_value(uint16_t value, uint8_t led, uint8_t first); // function declaration
+
+#endif /* SCT_H_ */
---
+++ zkouska/Core/Src/sct.c
@@ -0,0 +1,110 @@
+/*
+ * sct.c
+ *
+ *  Created on: 12. 10. 2022
+ *      Author: xpodan00
+ */
+
+#include "main.h" // including header file
+#include "sct.h" // including header file
+
+void sct_init(void) {
+	sct_led(0); // calling function sct_led() with argument (value) 0
+}
+
+void sct_led(uint32_t value) {
+	for (uint8_t i = 0; i < 32; i++) {
+		HAL_GPIO_WritePin(SCT_SDI_GPIO_Port, SCT_SDI_Pin, value & 1);
+		// returns the least significant bit LSB of value
+		// and it is set on SCT_SDI pin
+
+		/*Generates one CLK pulse*/
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 1);
+		HAL_GPIO_WritePin(SCT_CLK_GPIO_Port, SCT_CLK_Pin, 0);
+
+		value >>= 1; // bit shift to the right (ex.: value before 0b00001011
+		// value after 0b00000101
+	}
+	/*Generates one pulse on /LA */
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 1);
+	HAL_GPIO_WritePin(SCT_NLA_GPIO_Port, SCT_NLA_Pin, 0);
+}
+
+void sct_value(uint16_t value, uint8_t led, uint8_t first) {
+	uint32_t reg = 0;
+	static const uint32_t reg_values[5][10] = {
+			{
+					//PCDE--------GFAB @ DIS1
+					0b0111000000000111 << 16,
+					0b0100000000000001 << 16,
+					0b0011000000001011 << 16,
+					0b0110000000001011 << 16,
+					0b0100000000001101 << 16,
+					0b0110000000001110 << 16,
+					0b0111000000001110 << 16,
+					0b0100000000000011 << 16,
+					0b0111000000001111 << 16,
+					0b0110000000001111 << 16,
+			},
+			{
+					//----PCDEGFAB---- @ DIS2
+					0b0000011101110000 << 0,
+					0b0000010000010000 << 0,
+					0b0000001110110000 << 0,
+					0b0000011010110000 << 0,
+					0b0000010011010000 << 0,
+					0b0000011011100000 << 0,
+					0b0000011111100000 << 0,
+					0b0000010000110000 << 0,
+					0b0000011111110000 << 0,
+					0b0000011011110000 << 0,
+			},
+			{
+					//PCDE--------GFAB @ DIS3
+					0b0111000000000111 << 0,
+					0b0100000000000001 << 0,
+					0b0011000000001011 << 0,
+					0b0110000000001011 << 0,
+					0b0100000000001101 << 0,
+					0b0110000000001110 << 0,
+					0b0111000000001110 << 0,
+					0b0100000000000011 << 0,
+					0b0111000000001111 << 0,
+					0b0110000000001111 << 0,
+			},
+			{
+					//----43215678---- @ LED
+					0b0000001100000000 << 16,
+					0b0000011100000000 << 16,
+					0b0000111000000000 << 16,
+					0b0000110010000000 << 16,
+					0b0000100011000000 << 16,
+					0b0000000011100000 << 16,
+					0b0000000001110000 << 16,
+					0b0000000000110000 << 16,
+					0b0000000000010000 << 16,
+			},
+			{
+					//----43215678---- @ LED
+					0b0000000100000000 << 16,
+					0b0000011100000000 << 16,
+					0b0000111000000000 << 16,
+					0b0000110010000000 << 16,
+					0b0000100011000000 << 16,
+					0b0000000011100000 << 16,
+					0b0000000001110000 << 16,
+					0b0000000000110000 << 16,
+					0b0000000000010000 << 16,
+			}
+	};
+	reg |= reg_values[0][value / 100 % 10]; // set DIS3 number most right
+	reg |= reg_values[1][value / 10 % 10]; // set DIS2 number
+	reg |= reg_values[2][value / 1 % 10]; // set DIS1 number most left
+	if (first == 1) {
+		reg |= reg_values[4][led]; // set barograph values
+	}
+	else {
+		reg |= reg_values[3][led]; // set barograph values
+	}
+	sct_led(reg); // sends the value to the display - reg includes all useful numbers
+}
